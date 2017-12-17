79c6f7202376ae0814761bbfc78eca30837664b3
Bilal ayachi <bilal.ayachi@hetmml.nl>
2017-12-17 11:12:48 +0100

    Fix big #1

--- game/game.js
+++ game/game.js
@@ -3,15 +3,15 @@ var background;
 
 function preload() {
       background = loadImage('/game/asset/background.jpg');
-
 }
 
 
 
 function setup() {
+  createCanvas(800,600);
 
 }
 
 function draw() {
-
+  background(51);
 }
