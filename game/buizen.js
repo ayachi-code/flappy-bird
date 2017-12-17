4c63ac57eb223a75ce5b779fc520037c97a26cc6
Bilal ayachi <bilal.ayachi@hetmml.nl>
2017-12-17 17:22:05 +0100

    Bovenste buis veranderd van vorm

--- game/buizen.js
+++ game/buizen.js
@@ -7,14 +7,31 @@ function buizen() {
   this.buisboven_x2 = width/2;
   this.buisboven_y2 = height-120-30;
 
+  this.speed = 3;
+
 
 
   this.show = function () {
-        rect(this.buisboven_x,this.buisboven_y,30,120)
+        rect(this.buisboven_x,this.buisboven_y,30,this.g1)
         rect(this.buisboven_x2,this.buisboven_y2,30,120)
   }
 
-  
+  this.update = function() {
+      this.buisboven_x -= this.speed;
+      this.buisboven_x2 -= this.speed;
+
+      if (this.buisboven_x <= 0 || this.buisboven_x2 <= 0) {
+          console.log("Nieuwe buis");
+          this.buisboven_x = 400;
+          this.buisboven_x2 = 400;
+
+          this.g1 = random(200);
+
+
+
+      }
+
+  }
 
 
 
--- game/game.js
+++ game/game.js
@@ -24,6 +24,7 @@ function draw() {
   grond.show();
   vogel.show()
   buizen.show();
+  buizen.update();
   if (begin == true) {
       vogel.update();
   }
