5ece42b362a8d52088d8718568a7c24cb31030c2
Bilal ayachi <bilal.ayachi@hetmml.nl>
2017-12-19 11:09:07 +0100

    De programma weet welke vogel hij is door middel van een teller :)

--- game.js
+++ game.js
@@ -1,3 +1,5 @@
+var welke;
+
 var button = document.getElementById('speel').addEventListener('click',() => {
   window.location.href = '/game/index.html'
 });
@@ -9,3 +11,11 @@ var rscore = document.getElementById('highscore');
 
 
 rscore.innerHTML = "Jouw recenten score was " + descore;
+
+
+var vogel_bom = document.getElementById('bomv').addEventListener('click',() => {
+  welke = 2;
+  localStorage.setItem("welke-vogel",welke)
+  console.log(welke);
+
+});
