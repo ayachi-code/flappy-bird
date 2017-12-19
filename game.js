6de3263657169d019db3780b463c49a61b53923a
Bilal ayachi <bilal.ayachi@hetmml.nl>
2017-12-19 10:55:51 +0100

    Laat recenten score zien

--- game.js
+++ game.js
@@ -1,3 +1,11 @@
 var button = document.getElementById('speel').addEventListener('click',() => {
   window.location.href = '/game/index.html'
 });
+
+
+var descore = localStorage.getItem("score");
+
+var rscore = document.getElementById('highscore');
+
+
+rscore.innerHTML = "Jouw recenten score was " + descore;
