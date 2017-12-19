22783cccd51d5897c49b2646016ea39117a3ef08
Bilal ayachi <bilal.ayachi@hetmml.nl>
2017-12-19 10:26:40 +0100

    Firebase geinitialisserd en apikey etc toegevoegt

--- game/gameover/index.js
+++ game/gameover/index.js
@@ -1,3 +1,14 @@
 var restart_knop = document.getElementById('restart').addEventListener('click',() => {
   window.location.href = '/'
 })
+
+
+var config = {
+  apiKey: "AIzaSyAQB5Ad30sgyUUjlXoEt7pZuEfwTXupGAY",
+  authDomain: "flappy-bird-e8e40.firebaseapp.com",
+  databaseURL: "https://flappy-bird-e8e40.firebaseio.com",
+  projectId: "flappy-bird-e8e40",
+  storageBucket: "",
+  messagingSenderId: "527469947954"
+};
+firebase.initializeApp(config);
